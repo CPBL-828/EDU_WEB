067100dcd26360ec712ccfbba694739814496770
sinbi <snbiiiii1024@gmail.com>
2022-10-13 15:01:29 +0900

    주석 추가

--- src/views/UserSelectPage.vue
+++ src/views/UserSelectPage.vue
@@ -30,6 +30,11 @@ export default defineComponent({
       },
     ]);
 
+    /*
+    @brief 로그인 화면으로 이동
+    @date 22/10/06
+    @param 로그인 유저를 표시할 userType의 key 값
+     */
     const goLogin = (key: string) => {
       router.push("/" + key);
     };

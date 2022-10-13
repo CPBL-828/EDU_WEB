28b158ddca9ab11e43660d8e6ed14ab808363842
sinbi <snbiiiii1024@gmail.com>
2022-10-13 17:32:24 +0900

    sidebar 작업 중

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref, watch } from "vue";
+import SidebarComponent from "./components/SidebarComponent.vue";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
+  components: { SidebarComponent },
   setup() {
+    const route = useRoute();
+    const sidebarState = ref(false);
     onMounted(() => {
       console.log(
         "  ハ____ハ      ｡ﾟﾟ･｡･ﾟﾟ｡\n" +
-          " ꒰ ⬩ ω ⬩ ꒱    ˚｡     ｡˚\n" +
+          " ꒰ ⬩ ω ⬩ ꒱    ˚｡    ｡˚\n" +
           " |   つ Success! ﾟ･｡･ﾟ\n"
       );
+      console.log(route.path);
     });
+
+    watch(
+      () => route.path,
+      () => {
+        if (route.path !== "/" && route.path.substring(1, 6) !== "login") {
+          sidebarState.value = true;
+        }
+      }
+    );
+
+    return {
+      sidebarState,
+    };
   },
 });
 </script>
 
 <template>
+  <sidebar-component v-if="sidebarState"></sidebar-component>
   <router-view></router-view>
 </template>
 
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+import { KEYS } from "../constant";
+import CategoryItem from "./CategoryItem.vue";
+export default defineComponent({
+  name: "SidebarComponent",
+  components: { CategoryItem },
+  setup() {
+    onMounted(() => {});
+    return {};
+  },
+});
+</script>
+
+<template>
+  <section class="sidebar">
+    <div class="sidebar">
+      <div class="sidebar-user"></div>
+      <div class="sidebar-category">
+        <category-item></category-item>
+      </div>
+    </div>
+  </section>
+</template>

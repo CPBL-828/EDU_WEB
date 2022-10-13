87d7370dae09fbaff331f45c52fdc803dc98320a
sinbi <snbiiiii1024@gmail.com>
2022-10-13 18:09:10 +0900

    sidebar MainCategory

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -1,26 +1,17 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, PropType, ref } from "vue";
 import { categoryInterface } from "../lib/types";
-import { KEYS, USER_KEY } from "../constant";
-import { TEA_MAIN, TEA_SUB } from "../dummyCategory";
 export default defineComponent({
   name: "CategoryItem",
+  props: {
+    mainCategory: {
+      type: Array as PropType<Array<categoryInterface>>,
+      required: true,
+    },
+  },
   setup() {
-    //TODO userInterface
-    const userData = ref({ userType: "" });
-    const mainItem = ref<categoryInterface[]>();
-    const subItem = ref<categoryInterface[]>();
-
-    onMounted(() => {
-      userData.value = JSON.parse(localStorage.getItem(KEYS.LU));
-      if (userData.value?.userType === USER_KEY.TEA) {
-        mainItem.value = TEA_MAIN;
-        subItem.value = TEA_SUB;
-      }
-    });
-    return {
-      mainItem,
-    };
+    onMounted(() => {});
+    return {};
   },
 });
 </script>
@@ -28,7 +19,11 @@ export default defineComponent({
 <template>
   <section>
     <div class="category">
-      <div class="category-item" v-for="item in mainItem">
+      <div
+        class="category-item"
+        v-for="item in mainCategory"
+        @click="$emit('selectMain', item)"
+      >
         <i :class="item.ICON"></i>
         {{ item.VALUE }}
       </div>
--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,23 +1,52 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import { KEYS } from "../constant";
+import { KEYS, USER_KEY } from "../constant";
 import CategoryItem from "./CategoryItem.vue";
+import { categoryInterface } from "../lib/types";
+import { TEA_MAIN, TEA_SUB } from "../dummyCategory";
 export default defineComponent({
   name: "SidebarComponent",
   components: { CategoryItem },
   setup() {
-    onMounted(() => {});
-    return {};
+    const userData = ref({ userType: "" });
+    const mainItem = ref<categoryInterface[]>();
+    const subItem = ref<categoryInterface[]>();
+    const resultItem = ref<categoryInterface>();
+
+    const selectMain = (main: categoryInterface) => {
+      subItem.value?.map((item: categoryInterface) => {
+        if (main.KEY === item.KEY) resultItem.value = item;
+      });
+
+      console.log(resultItem.value?.VALUE);
+    };
+
+    onMounted(() => {
+      //TODO userData localStorage에서 받아오기
+      userData.value = JSON.parse(localStorage.getItem(KEYS.LU));
+      if (userData.value?.userType === USER_KEY.TEA) {
+        mainItem.value = TEA_MAIN;
+        subItem.value = TEA_SUB;
+      }
+    });
+    return {
+      mainItem,
+      subItem,
+      selectMain,
+    };
   },
 });
 </script>
 
 <template>
   <section class="sidebar">
-    <div class="sidebar">
+    <div class="sidebar" v-if="mainItem">
       <div class="sidebar-user"></div>
       <div class="sidebar-category">
-        <category-item></category-item>
+        <category-item
+          :main-category="mainItem"
+          @selectMain="selectMain"
+        ></category-item>
       </div>
     </div>
   </section>

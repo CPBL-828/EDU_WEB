69007ae3ac6dd17ec4f77ec3103d3f21ea1f0caf
ssso-h <91714387@jmail.ac.kr>
2022-10-21 11:29:08 +0900

    내 정보 페이지 작업 중

---
+++ src/components/MyInfoComponent.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+import { adminInterface, teacherInterface } from "../lib/types";
+export default defineComponent({
+  name: "MyInfoComponent",
+  props: {
+    userData: {
+      type: Object as PropType<teacherInterface | adminInterface>,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <section class="my-info">내 정보!!!!!!</section>
+</template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,13 +1,29 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import MyInfoComponent from "../components/MyInfoComponent.vue";
+import { adminInterface, teacherInterface } from "../lib/types";
+import common from "../lib/common";
+import { KEYS } from "../constant";
 export default defineComponent({
   name: "MainPage",
+  components: { MyInfoComponent },
   setup() {
-    return {};
+    const userData = ref<teacherInterface | adminInterface>();
+
+    onMounted(() => {
+      userData.value = common.getItem(KEYS.LU);
+    });
+    return {
+      userData,
+    };
   },
 });
 </script>
 
 <template>
-  <section>main</section>
+  <section>
+    <my-info-component
+      :user-data="userData !== undefined ? userData : {}"
+    ></my-info-component>
+  </section>
 </template>

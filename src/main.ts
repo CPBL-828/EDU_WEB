164dc9df28af20253a9695841e02d7eac4359000
sinbi <snbiiiii1024@gmail.com>
2022-10-22 17:20:28 +0900

    vuex store 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import store from "./store";
 import router from "./router";
 import "v-calendar/dist/style.css";
 import VCalendarLibrary from "v-calendar";
 import VueClickAwayPlugin from "vue3-click-away";
 
 createApp(App)
+  .use(store)
   .use(router)
   .use(VCalendarLibrary, {})
   .use(VueClickAwayPlugin)

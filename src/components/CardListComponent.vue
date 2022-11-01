c09c7fac341766bb568bf8676ca995ddc32a1ae3
sinbi <snbiiiii1024@gmail.com>
2022-11-02 00:28:36 +0900

    카드형 명단 컴포넌트 추가

---
+++ src/components/CardListComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "CardListComponent",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <section class="card">카드형~</section>
+</template>

edfa6787731554ce643d03d0d95c922175a523bd
ssso-h <91714387@jmail.ac.kr>
2022-11-04 18:16:29 +0900

    파일 생성 작업 중

---
+++ src/components/scheduleManage/CurrentScheduleComponent.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "CurrentScheduleComponent.vue",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>현재 시간표</template>
---
+++ src/views/scheduleManage/ScheduleManagePage.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent, onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import CurrentScheduleComponent from "../../components/scheduleManage/CurrentScheduleComponent.vue";
+
+export default defineComponent({
+  name: "ScheduleManagePage.vue",
+  components: { CurrentScheduleComponent },
+  setup() {
+    const route = useRoute();
+    const currentCategory = ref<string | undefined>(undefined);
+
+    watch(
+      () => route.path,
+      () => {
+        currentCategory.value = route.path.split("/")[2]?.toUpperCase();
+      }
+    );
+
+    onMounted(async () => {
+      currentCategory.value = route.path.split("/")[2]?.toUpperCase();
+    });
+
+    return {
+      currentCategory,
+    };
+  },
+});
+</script>
+
+<template>
+  <!--  <current-schedule-component.vue-->
+  <!--    v-if="currentCategory === 'CURRENT'"-->
+  <!--  ></current-schedule-component.vue>-->
+</template>

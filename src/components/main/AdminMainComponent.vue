b8423918fe4a34a9060dde4d9daa3a4c02e60f49
sinbi <snbiiiii1024@gmail.com>
2023-05-12 18:20:40 +0900

    [관리자] 메인 페이지 강의 상세 모달

--- src/components/main/AdminMainComponent.vue
+++ src/components/main/AdminMainComponent.vue
@@ -6,14 +6,17 @@ import { KEYS, USER_KEY } from "../../constant";
 import { ApiClient } from "../../axios";
 import { defaultInterface, suggestInterface } from "../../lib/types";
 import DataListComponent from "../custom/DataListComponent.vue";
+import ModalPopupComponent from "../custom/ModalPopupComponent.vue";
+import { useStore } from "vuex";
 /*
 @brief [관리자] 로그인 성공 후 가장 초기 메인 화면
        강사와 학생의 건의 목록은 양쪽에 표시
  */
 export default defineComponent({
   name: "AdminMainComponent",
-  components: { DataListComponent, ConsultComponent },
+  components: { ModalPopupComponent, DataListComponent, ConsultComponent },
   setup() {
+    const store = useStore();
     const userKey = ref<string | undefined>(undefined);
     const header: defaultInterface[] = [
       { KEY: "DATE", VALUE: "건의 일자" },
@@ -24,6 +27,7 @@ export default defineComponent({
     const stuTotal = ref<number>(0);
     const teacherSuggest = ref<Array<suggestInterface> | undefined>(undefined);
     const teaTotal = ref<number>(0);
+    const suggestDetail = ref<suggestInterface | undefined>(undefined);
 
     const setStudentSuggest = async () => {
       let data = {
@@ -73,6 +77,12 @@ export default defineComponent({
       }
     };
 
+    const showSuggestDetail = (s: suggestInterface) => {
+      suggestDetail.value = s;
+
+      store.commit("setModalState", true);
+    };
+
     onMounted(() => {
       if (common.getItem(KEYS.LU)) {
         userKey.value = common.getItem(KEYS.LU).adminKey;
@@ -88,6 +98,8 @@ export default defineComponent({
       stuTotal,
       teacherSuggest,
       teaTotal,
+      suggestDetail,
+      showSuggestDetail,
     };
   },
 });
@@ -109,6 +121,7 @@ export default defineComponent({
                 :total-cnt="stuTotal"
                 :list-cnt="8"
                 admin-state="Y"
+                @saveSuggestDetail="showSuggestDetail"
               >
               </data-list-component>
             </div>
@@ -126,6 +139,7 @@ export default defineComponent({
                 :total-cnt="teaTotal"
                 :list-cnt="8"
                 admin-state="Y"
+                @saveSuggestDetail="showSuggestDetail"
               >
               </data-list-component>
             </div>
@@ -133,5 +147,68 @@ export default defineComponent({
         </div>
       </div>
     </div>
+
+    <modal-popup-component title="건의 내용">
+      <template v-slot:body>
+        <div class="suggest-detail">
+          <span class="suggest-detail-tip">
+            답변을 작성 또는 수정하시려면 학생/강사 건의사항 서비스로 이동해
+            주세요.
+          </span>
+          <div class="suggest-detail-section">
+            <div class="suggest-detail-section-header">
+              <div class="date">
+                {{ suggestDetail?.createDate.substring(0, 4) }}년
+                {{ suggestDetail?.createDate.substring(5, 7) }}월
+                {{ suggestDetail?.createDate.substring(8, 10) }}일
+              </div>
+              <div class="sap"></div>
+              <div class="type">{{ suggestDetail?.type }}</div>
+              <div class="sap"></div>
+              <div class="state">
+                {{ suggestDetail?.state === "Y" ? "처리 완료" : "대기 중" }}
+              </div>
+              <div class="sap"></div>
+              <div class="writer">{{ suggestDetail?.writerName }}</div>
+            </div>
+            <div class="top-sap"></div>
+            <div class="suggest-detail-section-content">
+              <div class="suggest-detail-section-content-label">건의 내용</div>
+              <div class="suggest-detail-section-content-item">
+                {{ suggestDetail?.content }}
+              </div>
+            </div>
+            <div class="center-sap"></div>
+            <div class="suggest-detail-section-answer">
+              <div class="suggest-detail-section-answer-label">
+                작성한 답변<span
+                  >(답변일자:
+                  {{
+                    suggestDetail?.answerDate
+                      ? suggestDetail?.answerDate.substring(0, 4) +
+                        "년 " +
+                        suggestDetail?.answerDate.substring(5, 7) +
+                        "월 " +
+                        suggestDetail?.answerDate.substring(8, 10) +
+                        "일"
+                      : ""
+                  }})</span
+                >
+              </div>
+              <div
+                style="white-space: pre"
+                class="suggest-detail-section-answer-item"
+              >
+                {{
+                  suggestDetail?.answer
+                    ? suggestDetail?.answer
+                    : "작성한 답변이 없습니다."
+                }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </modal-popup-component>
   </section>
 </template>
